477d871323a0fe240f5d1edd7656bc0521aea58f
Pedro Filipe Costa <pedro.filipe@talkdesk.com>
2022-02-03 10:35:35 +0000

    added todo

--- tdd-app/src/helpers/pokemons.ts
+++ tdd-app/src/helpers/pokemons.ts
@@ -15,5 +15,6 @@ export type Pokemon = {
 };
 
 export async function getPokemons(): Promise<Pokemon[]> {
+  // TODO - "https://gist.githubusercontent.com/jherr/23ae3f96cf5ac341c98cd9aa164d2fe3/raw/f8d792f5b2cf97eaaf9f0c2119918f333e348823/pokemon.json"
   return Promise.reject("Not implemented");
 }
